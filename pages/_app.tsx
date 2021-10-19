93803366fc70b7dc8dde16621f58b5c1dd36a8c7
Rob <robzlegz@gmail.com>
2021-10-19 19:06:16 +0300

    scss setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import '../styles/globals.css'
+import '../styles/main.scss'
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
