c4774f222d9263d7648c32f3a7e3ec3390d50978
Rob <robzlegz@gmail.com>
2021-10-21 09:30:11 +0300

    socket connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import Head from "next/head"
-import Notification from "../components/notifications/Notification";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connect, getSocket, selectSocket } from "../redux/slices/socketSlice";
 
 export default function Home() {
+  const socketInfo = useSelector(selectSocket);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(!socketInfo.connected){
+      dispatch(connect("/api/socket"));
+    }
+  }, [socketInfo.connected]);
+
   return (
     <div>
       <Head>
         <title>MathRoom</title>
       </Head>
-      
-      <Notification />
     </div>
   )
 }
