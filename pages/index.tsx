ac6cb4fbcac6e839f803e638f660e02414324cb2
Rob <robzlegz@gmail.com>
2021-10-21 09:37:25 +0300

    socket connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from "next/head"
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { connect, getSocket, selectSocket } from "../redux/slices/socketSlice";
+import { connect, getOnlineUsers, getSocket, selectSocket } from "../redux/slices/socketSlice";
+import { selectUser } from "../redux/slices/userSlice";
+
+interface User{
+  userId: string;
+  socketId: string;
+}
 
 export default function Home() {
   const socketInfo = useSelector(selectSocket);
+  const userInfo = useSelector(selectUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if(!socketInfo.connected){
-      dispatch(connect("/api/socket"));
+      dispatch(connect("/api/socket.io"));
+    }else if(socketInfo.connected && userInfo.info){
+      const socket = getSocket();
+      socket.emit("addUser", userInfo.info._id);
+      socket.on("getUsers", (users: User[]) => {
+        dispatch(getOnlineUsers(users));
+      });
     }
   }, [socketInfo.connected]);
 
