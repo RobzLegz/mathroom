04c42514aa73da320257b6ae3db95046dab5066b
Rob <robzlegz@gmail.com>
2021-10-23 13:41:56 +0300

    deleting useless code

--- pages/rooms/[id].tsx
+++ pages/rooms/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import Head from "next/head"
 import { useDispatch, useSelector } from "react-redux";
 import { getRoomInfo } from "../../requests/rooms/requests";
