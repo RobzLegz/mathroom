b8adcda1e758813c6a1c50e73ad827706f67b2ec
Rob <robzlegz@gmail.com>
2021-10-24 02:56:23 +0300

    #115 admin room options

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import { useRouter } from "next/dist/client/router";
 import Head from "next/head"
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSocket, selectSocket, setSocket } from "../redux/slices/socketSlice";
+import { selectUser } from "../redux/slices/userSlice";
+import { checkForLogin } from "../requests/auth/requests";
 
 export default function Home() {
+  const userInfo = useSelector(selectUser);
   const socketInfo = useSelector(selectSocket);
   const dispatch = useDispatch();
+  const router = useRouter();
 
   useEffect(() => {
     const socket = getSocket();
@@ -14,6 +19,16 @@ export default function Home() {
       dispatch(setSocket(true));
     }
   }, [socketInfo.connected]);
+
+  useEffect(() => {
+    if(!userInfo.loggedIn || !userInfo.token){
+      const token = window.localStorage.getItem("refreshtoken");
+
+      if(token){
+        checkForLogin(dispatch, router);
+      }
+    }
+}, [userInfo.loggedIn, dispatch, userInfo.token, router]);
 
   return (
     <div>
