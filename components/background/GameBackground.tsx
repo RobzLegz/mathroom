c0314bcecb3c6f636736a68e72ee037fc043b3d4
Rob <robzlegz@gmail.com>
2021-10-30 02:50:56 +0300

    bg component

---
+++ components/background/GameBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props{
+    color: string;
+}
+
+const GameBackground: React.FC<Props> = ({color}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GameBackground
