d4001908f448050e8c42e5ce3a21a30d07a92999
Rob <robzlegz@gmail.com>
2021-10-30 00:36:54 +0300

    setting up components

--- components/containers/menu/MenuContainer.tsx
+++ components/containers/menu/MenuContainer.tsx
@@ -1,9 +1,21 @@
+import { useRouter } from 'next/dist/client/router';
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../../redux/slices/userSlice';
 
 function MenuContainer() {
+    const userInfo = useSelector(selectUser);
+    const router = useRouter();
+
     return (
         <div className="menu__container">
-            
+            <h1>MATHROOM</h1>
+
+            <div className="menu__container__options">
+                <button onClick={() => router.push("/rooms")}>Play</button>
+                <button onClick={() => router.push("/instructions")}>Instructions</button>
+                {!userInfo.loggedIn || !userInfo.token && (<button onClick={() => router.push("/auth/login")}>Authorize</button>)}
+            </div>
         </div>
     )
 }
---
+++ pages/instructions.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import Notification from "../components/notifications/Notification";
+
+export default function Instructions() {
+  return (
+    <div>
+      <Head>
+        <title>MathRoom | Instructions</title>
+      </Head>
+      <Notification />
+    </div>
+  )
+}
