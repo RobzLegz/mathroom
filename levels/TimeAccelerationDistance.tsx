d9ba78aeb7c0cbe85b913ccefad32b375637f23f
Rob <robzlegz@gmail.com>
2021-10-31 23:16:24 +0200

    new level

---
+++ levels/TimeAccelerationDistance.tsx
@@ -0,0 +1,89 @@
+import { useRouter } from 'next/dist/client/router';
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { setNotification } from '../redux/slices/notificationSlice';
+import { selectUser } from '../redux/slices/userSlice';
+import { nextLevel } from '../requests/levels/requests';
+
+const TimeAccelerationDistance: React.FC = () => {
+    const userInfo = useSelector(selectUser);
+
+    const [selectedAge, setSelectedAge] = useState<number>(5);
+    const [startingSpeed] = useState<number>(Math.floor((Math.random() * 22) + 7));
+    const [acceleration] = useState<number>(Math.floor((Math.random() * 5) + 2));
+    const [time] = useState<number>(Math.floor((Math.random() * 7) + 2));
+    const [finalSpeed] = useState<number>(startingSpeed + acceleration * time);
+    const [needHelp, setNeedHelp] = useState<boolean>(false);
+    const [writing, setWriting] = useState<boolean>(false);
+
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    const {level} = router.query;
+
+    const completeLevel = (e: any) => {
+        e.preventDefault();
+
+        if(selectedAge !== acceleration){
+            return dispatch(setNotification({type: "error", message: "Incorrect answer!"}));
+        }
+
+        if(userInfo.info){
+            if(userInfo.info.level > Number(level)){
+                return router.push(`/levels/${Number(level) + 1}`)
+            }
+
+            if(userInfo.level < Number(level)){
+                dispatch(setNotification({type: "error", message: "You can't be on this level"}));
+                return router.push("/levels")
+            }
+
+            nextLevel(Number(level), userInfo.token, router, dispatch);
+        }
+    }
+
+    return (
+        <form className="level__age level__container">
+            {needHelp && (
+                <div className="level__container__tip">
+                    <p></p>
+                </div>
+            )}
+            
+            <div className="level__container__task">
+                <strong>In smoothly accelerated motion, the final speed of the body is {finalSpeed} m/s, but the starting speed is {startingSpeed} m/s. The acceleration happened in {time} seconds. Calculate the acceleration.<img src="/svg/question.svg" alt="question mark inside circle" onClick={() => setNeedHelp(!needHelp)} /></strong>
+            </div>
+            <div className="level__container__options">
+                <div className="level__container__options__tools">
+                    <div className="inputContainer">
+                        {writing ? (
+                            <input 
+                                type="number" 
+                                value={selectedAge.toString()}
+                                onChange={(e) => {if(e.target.value.length > 3){return}setSelectedAge(Number(e.target.value))}}
+                            />
+                        ) : (
+                            <input 
+                                type="range" 
+                                value={selectedAge}
+                                onChange={(e) => setSelectedAge(Number(e.target.value))}
+                                min="0"
+                                max="10"
+                            />
+                        )}
+                        <strong onClick={() => setWriting(!writing)}>{selectedAge}</strong>
+                    </div>
+                    <div className="level__container__options__tools__instruction">
+                        <small>Slide from left to right to change value or click the number next to it to write result</small>
+                    </div>
+                    <button className="level__container__options__tools__submit" onClick={(e) => completeLevel(e)}>Submit</button>
+                </div>
+                <div className="level__container__options__ilustration">
+                    <img src="https://tse3.mm.bing.net/th?id=OIP.wSFbjLZV5FropOTZA0gnWgHaE7&pid=Api" alt="sus" />
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default TimeAccelerationDistance
