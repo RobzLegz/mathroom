14dc5fe690d252a21937ac1ae9b8ded9e2e7cb0d
Rob <robzlegz@gmail.com>
2021-10-31 12:07:59 +0200

    ilustration

--- levels/TypeAge.tsx
+++ levels/TypeAge.tsx
@@ -6,20 +6,26 @@ interface Props{
 
 const TypeAge: React.FC<Props> = ({description}) => {
     const [selectedAge, setSelectedAge] = useState<number>(50);
-    console.log(selectedAge)
 
     return (
         <div className="level__age level__container">
             <div className="level__container__tip">
                 <p>{description}</p>
             </div>
-            <input 
-                type="range" 
-                value={selectedAge}
-                onChange={(e) => setSelectedAge(Number(e.target.value))}
-                min="5"
-                max="100"
-            />
+            <div className="level__container__task">
+                <div className="level__container__task__tools">
+                    <input 
+                        type="range" 
+                        value={selectedAge}
+                        onChange={(e) => setSelectedAge(Number(e.target.value))}
+                        min="5"
+                        max="100"
+                    />
+                </div>
+                <div className="level__container__task__ilustration">
+                    <img src="https://tse3.mm.bing.net/th?id=OIP.wSFbjLZV5FropOTZA0gnWgHaE7&pid=Api" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
