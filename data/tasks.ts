55c0676365c622b28f2463bcc84747dd30e871f5
Rob <robzlegz@gmail.com>
2021-11-01 00:03:34 +0200

    artifitially raising task amount

--- data/tasks.ts
+++ data/tasks.ts
@@ -1,79 +1,33 @@
 const tasks = [
-    {
-        type: "age",
-    },
-    {
-        type:"time sum",
-    },
-    {
-        type: "time difference",
-    },
-    {
-        type: "time difference minutes",
-    },
-    {
-        type: "time difference minutes2",
-    },
-    {
-        type: "month amount",
-    },
-    {
-        type: "day amount",
-    },
-    {
-        type: "second amount",
-    },
-    {
-        type: "roman numerals",
-    },
-    {
-        type: "kilometers apart",
-    },
-    {
-        type: "road calculation",
-    },
-    {
-        type: "time calculation",
-    },
-    {
-        type: "road calculation2",
-    },
-    {
-        type: "m/min to m/h",
-    },
-    {
-        type: "speed calculation",
-    },
-    {
-        type: "acceleration calculation",
-    },
-    {
-        type: "uniformly slow motion",
-    },
-    {
-        type: "acceleration calculation",
-    },
-    {
-        type: "time from acceleration + distance",
-    },
-    {
-        type: "Δt",
-    },
-    {
-        type: "average speed",
-    },
-    {
-        type: "average speed",
-    },
-    {
-        type: "average speed",
-    },
-    {
-        type: "distance between",
-    },
-    {
-        type: "time from acceleration + distance",
-    },
+    {type: "age"},
+    {type:"time sum"},
+    {type: "time difference"},
+    {type: "time difference minutes"},
+    {type: "time difference minutes2"},
+    {type: "month amount"},
+    {type: "day amount"},
+    {type: "second amount"},
+    {type: "roman numerals"},
+    {type: "kilometers apart"},
+    {type: "road calculation"},
+    {type: "time calculation"},
+    {type: "speed calculation"},
+    {type: "road calculation2"},
+    {type: "time calculation"},
+    {type: "m/min to m/h"},
+    {type: "speed calculation"},
+    {type: "acceleration calculation"},
+    {type: "uniformly slow motion"},
+    {type: "acceleration calculation"},
+    {type: "uniformly slow motion"},
+    {type: "acceleration calculation"},
+    {type: "time from acceleration + distance"},
+    // {type: "Δt"},
+    // {type: "average speed"},
+    // {type: "average speed"},
+    // {type: "average speed"},
+    // {type: "distance between"},
+    // {type: "time from acceleration + distance"},
 ];
 
 export default tasks;
