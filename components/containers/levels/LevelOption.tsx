77030b16d359b39d1430226f8d4cb50d9f7bf061
Rob <robzlegz@gmail.com>
2021-10-31 11:05:10 +0200

    dont allow user to get in a level

--- components/containers/levels/LevelOption.tsx
+++ components/containers/levels/LevelOption.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/dist/client/router";
 import React from "react"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setNotification } from "../../../redux/slices/notificationSlice";
 import { selectUser } from "../../../redux/slices/userSlice";
 
 interface Task{
@@ -19,15 +20,20 @@ interface Props{
 
 const LevelOption: React.FC<Props> = ({task}) => {
     const userInfo = useSelector(selectUser);
+
+    const dispatch = useDispatch();
     const router = useRouter();
 
     if(userInfo.loggedIn){
         return (
-            <div className={`levels__container__level levels__container__level__${Number(task.level) > 30 ? "purple" : Number(task.level) > 20 ? "red" : Number(task.level) > 10 ? "yellow" : "green"}`} onClick={() => router.push(`/levels/${task.level}`)}>
+            <div 
+                className={`levels__container__level levels__container__level__${Number(task.level) > 30 ? "purple" : Number(task.level) > 20 ? "red" : Number(task.level) > 10 ? "yellow" : "green"}`} 
+                onClick={() => {if(userInfo.info.level < Number(task.level)){return dispatch(setNotification({type: "error", message: "You haven't done the previous levels!"}))}router.push(`/levels/${task.level}`)}}
+            >
                 {userInfo.info.level >= Number(task.level) ? (
                     <h4>{task.level}</h4>
                 ) : (
-                    <img src="/svg/lock.svg" alt="" />
+                    <img src="/svg/lock.svg" alt="padlock" />
                 )}
             </div>
         )
