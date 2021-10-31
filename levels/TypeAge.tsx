5c9f4c4a57f3c7af2e82e7a8ccc50d655765529b
Rob <robzlegz@gmail.com>
2021-10-31 14:52:11 +0200

    type age almost finished

--- levels/TypeAge.tsx
+++ levels/TypeAge.tsx
@@ -5,24 +5,40 @@ interface Props{
 }
 
 const TypeAge: React.FC<Props> = ({description}) => {
-    const [selectedAge, setSelectedAge] = useState<number>(50);
+    const [selectedAge, setSelectedAge] = useState<number>(5);
+    const [correctAnswer] = useState<number>(Math.floor((Math.random() * 64) + 5));
+    const [needHelp, setNeedHelp] = useState<boolean>(false);
 
     return (
         <div className="level__age level__container">
-            <div className="level__container__tip">
-                <p>{description}</p>
-            </div>
-            <div className="level__container__task">
-                <div className="level__container__task__tools">
-                    <input 
-                        type="range" 
-                        value={selectedAge}
-                        onChange={(e) => setSelectedAge(Number(e.target.value))}
-                        min="5"
-                        max="100"
-                    />
+            {needHelp && (
+                <div className="level__container__tip">
+                    <p>To get Your current age, subtract given year from current year ({new Date().getFullYear()})</p>
                 </div>
-                <div className="level__container__task__ilustration">
+            )}
+            
+            <div className="level__container__task">
+                <strong>How old are You now if You were born in {new Date().getFullYear() - correctAnswer}?</strong>
+                <img src="/svg/question.svg" alt="question mark inside circle" onClick={() => setNeedHelp(!needHelp)} />
+            </div>
+            <div className="level__container__options">
+                <div className="level__container__options__tools">
+                    <div className="inputContainer">
+                        <input 
+                            type="range" 
+                            value={selectedAge}
+                            onChange={(e) => setSelectedAge(Number(e.target.value))}
+                            min="5"
+                            max="100"
+                        />
+                        <strong>{selectedAge}</strong>
+                    </div>
+                    <div className="level__container__options__tools__instruction">
+                        <small>Slide from left to right to change value</small>
+                    </div>
+                    <button className="level__container__options__tools__submit">Submit</button>
+                </div>
+                <div className="level__container__options__ilustration">
                     <img src="https://tse3.mm.bing.net/th?id=OIP.wSFbjLZV5FropOTZA0gnWgHaE7&pid=Api" alt="" />
                 </div>
             </div>
