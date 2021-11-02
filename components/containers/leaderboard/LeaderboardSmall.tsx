9b0436c83d173c0b91120d95503ca3c3baed3458
Rob <robzlegz@gmail.com>
2021-11-03 00:28:11 +0200

    leaderboard must be fixed

--- components/containers/leaderboard/LeaderboardSmall.tsx
+++ components/containers/leaderboard/LeaderboardSmall.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { selectRooms } from '../../../redux/slices/roomSlice';
 
@@ -14,11 +14,22 @@ interface RoomUser{
 const LeaderboardSmall: React.FC = () => {
     const roomInfo = useSelector(selectRooms);
 
+    const [leaders, setLeaders] = useState<RoomUser[] | null>(null);
+
+    useEffect(() => {
+        if(roomInfo && roomInfo.roomUsers){
+            setLeaders([...roomInfo.roomUsers].sort((a: RoomUser, b: RoomUser) => {return a.points-b.points}))
+        }
+    }, [roomInfo.roomUsers, roomInfo]);
+
+    if(!leaders){
+        return null;
+    }
+
     return (
         <div className="leaderboardSmall">
             {
-                roomInfo.roomUsers
-                    .sort((a: RoomUser, b: RoomUser) => {return a.points - b.points})
+                leaders                    
                     .map((object: RoomUser, i: number) => (
                         <div className="leaderboardSmall__object" key={i}>
                             <h4>#{i + 1}</h4>
