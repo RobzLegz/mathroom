e37a086b2a728efba926a2edfc83af6f0a53a5c6
Rob <robzlegz@gmail.com>
2021-11-02 23:46:01 +0200

    starting leaderboard component

---
+++ components/containers/leaderboard/Leaderboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Leaderboard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Leaderboard
---
+++ components/containers/leaderboard/LeaderboardSmall.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LeaderboardSmall() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LeaderboardSmall
