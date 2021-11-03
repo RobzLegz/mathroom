6b44982aa86ad80ce7d0503b38dca60d60403d5f
Rob <robzlegz@gmail.com>
2021-11-03 11:03:53 +0200

    small leaderboard

--- components/containers/leaderboard/Leaderboard.tsx
+++ components/containers/leaderboard/Leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux';
 import { selectRooms } from '../../../redux/slices/roomSlice';
 
