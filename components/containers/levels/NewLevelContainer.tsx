bbe97df5d087fb6d42c4355704279ae395d9b848
Rob <robzlegz@gmail.com>
2021-11-05 00:44:38 +0200

    new level container

--- components/containers/levels/NewLevelContainer.tsx
+++ components/containers/levels/NewLevelContainer.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function NewLevelContainer() {
+    const [preview, setPreview] = useState(false);
+
     return (
         <div className="communityPage__newLevelContainer">
-            
+            <header className="communityPage__newLevelContainer__header">
+                <h2>Create new level</h2>
+            </header>
+
+            {preview ? (
+                <>
+                    <div className="communityPage__newLevelContainer__preview">
+
+                    </div>
+
+                    <footer className="communityPage__newLevelContainer__previewFooter">
+
+                    </footer>
+                </>
+            ) : (
+                <>
+                    <div className="communityPage__newLevelContainer__body">
+
+                    </div>
+
+                    <footer className="communityPage__newLevelContainer__footer">
+                        <button onClick={() => setPreview(true)}>Preview</button>
+                    </footer>
+                </>
+            )}
         </div>
     )
 }
