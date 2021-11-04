6f47a85f787d224d312ef282d976f946c50afbc0
Rob <robzlegz@gmail.com>
2021-11-04 20:34:17 +0200

    instruction page

--- pages/instructions.tsx
+++ pages/instructions.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head"
+import InstructionContainer from "../components/containers/instructions/InstructionContainer";
 import Notification from "../components/notifications/Notification";
 
 export default function Instructions() {
   return (
-    <div>
+    <div className="instructionPage">
       <Head>
         <title>MathRoom | Instructions</title>
       </Head>
       
       <Notification />
+
+      <InstructionContainer />
     </div>
   )
 }
