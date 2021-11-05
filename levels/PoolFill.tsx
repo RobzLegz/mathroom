b71bd395a41b4c3fb31caa0263ef9f4832055254
sanvirus <91952766+Sanvirus@users.noreply.github.com>
2021-11-05 18:10:41 +0200

    8

--- levels/PoolFill.tsx
+++ levels/PoolFill.tsx
@@ -65,7 +65,7 @@ const PoolFill: React.FC<Props> = ({needHelp, setNeedHelp, multiplayer}) => {
                             <div className="line2"></div>
                         </div>
                         <div className="level__container__tip__inner__text">
-                            <p></p>
+                            <p>time is the volume divided by the filling speed</p>
                         </div>
                         <div className="buttonContainer">
                             <button onClick={() => setNeedHelp(false)}>Okay</button>
