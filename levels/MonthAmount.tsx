e9dd230ee68d8c5b3cbe7a5eeb7d891c5fe05522
sanvirus <91952766+Sanvirus@users.noreply.github.com>
2021-11-07 00:48:16 +0200

    Update MonthAmount.tsx

--- levels/MonthAmount.tsx
+++ levels/MonthAmount.tsx
@@ -65,7 +65,7 @@ const MonthAmount: React.FC<Props> = ({needHelp, setNeedHelp, multiplayer}) => {
                             <div className="line2"></div>
                         </div>
                         <div className="level__container__tip__inner__text">
-                            <p>One year is 12 months, multiply by years and then add months.</p>
+                            <p>One year is 12 months. Multiply years by 12 and then add months.</p>
                         </div>
                         <div className="buttonContainer">
                             <button onClick={() => setNeedHelp(false)}>Okay</button>
