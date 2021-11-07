9e7021caaabdfac75bdf10864f8fcb83b700cffa
Rob <robzlegz@gmail.com>
2021-11-07 19:42:00 +0200

    getting user levels

--- pages/api/user/profile/[username].ts
+++ pages/api/user/profile/[username].ts
@@ -1,5 +1,6 @@
 import connectDB from "../../../../utils/connectDB";
 import Users from "../../../../models/userModel";
+import Levels from "../../../../models/levelModel";
 import auth from "../../../../middleware/auth";
 
 connectDB();
@@ -21,7 +22,20 @@ const getUserInfo = async (req: any, res: any) => {
             return res.status(400).json({err: "Sorry, a user with this username doesn't exist!"});
         }
 
-        res.json(user);
+        const userLevels = await Levels.find({author: user._id.toString()});
+
+        const resUser = {
+            username: user.username,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            avatar: user.avatar,
+            level: user.level,
+            passedLevels: user.passedLevels,
+            userLevels: userLevels,
+        }
+
+        res.json(resUser);
     }catch(err: any){
         return res.status(500).json({err: err.message});
     }
