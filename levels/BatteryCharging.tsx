3b720f8d4890f82274e4acabc7c1262abd91e431
Rob <robzlegz@gmail.com>
2021-11-14 17:32:34 +0200

    tip update

--- levels/BatteryCharging.tsx
+++ levels/BatteryCharging.tsx
@@ -80,7 +80,7 @@ const BatteryCharging: React.FC<Props> = ({needHelp, setNeedHelp, multiplayer}) 
                             <div className="line2"></div>
                         </div>
                         <div className="level__container__tip__inner__text">
-                            <p>Divide 100 by charging speed to get the time.</p>
+                            <p>Divide 100 (full battery) by charging speed to get the time.</p>
                         </div>
                         <div className="buttonContainer">
                             <button onClick={() => setNeedHelp(false)}>Okay</button>
