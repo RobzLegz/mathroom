d9cf48c6152b72c4018cd654bebbe45fa8451c9a
Rob <robzlegz@gmail.com>
2021-11-17 22:13:42 +0200

    images

---
+++ data/customLevelImages.ts
@@ -0,0 +1,280 @@
+const customLevelImages = [
+    {
+        src: "/custom-level/bird.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/car.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/cat.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/clock.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/conffetty.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/dash.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/dog.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/farm.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/food.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/house.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/lightbolb.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/list.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/point-to-point.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/search.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/timer.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/trees.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/walkdog.png",
+        alt: ""
+    },
+    {
+        src: "/custom-level/water.png",
+        alt: ""
+    },
+    {
+        src: "/levels/battery0.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/battery1.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/battery2.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/battery3.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/battery4.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/battery5.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/burglar.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock1.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock2.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock3.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock4.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock5.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock6.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock7.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/clock8.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/grandmaWithCookies.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/kid.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/man.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/old.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/pool.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/poolHole.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/programmers.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/sunriseset.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/teenager.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/train.svg",
+        alt: ""
+    },
+    {
+        src: "/levels/bike_and_motorbike.png",
+        alt: ""
+    },
+    {
+        src: "/levels/calendar.png",
+        alt: ""
+    },
+    {
+        src: "/levels/car_and_motorbike.png",
+        alt: ""
+    },
+    {
+        src: "/levels/car1.png",
+        alt: ""
+    },
+    {
+        src: "/levels/car2.png",
+        alt: ""
+    },
+    {
+        src: "/levels/car3.png",
+        alt: ""
+    },
+    {
+        src: "/levels/cash.png",
+        alt: ""
+    },
+    {
+        src: "/levels/cookie.png",
+        alt: ""
+    },
+    {
+        src: "/levels/cyclist.png",
+        alt: ""
+    },
+    {
+        src: "/levels/dash.png",
+        alt: ""
+    },
+    {
+        src: "/levels/fly.png",
+        alt: ""
+    },
+    {
+        src: "/levels/frog.png",
+        alt: ""
+    },
+    {
+        src: "/levels/metro.png",
+        alt: ""
+    },
+    {
+        src: "/levels/pay.png",
+        alt: ""
+    },
+    {
+        src: "/levels/skaters.png",
+        alt: ""
+    },
+    {
+        src: "/png/car.png",
+        alt: ""
+    },
+    {
+        src: "/png/coin.png",
+        alt: ""
+    },
+    {
+        src: "/png/motorbike.png",
+        alt: ""
+    },
+    {
+        src: "/png/pictureIcon.png",
+        alt: ""
+    },
+    {
+        src: "/png/skater1.png",
+        alt: ""
+    },
+    {
+        src: "/png/skater2.png",
+        alt: ""
+    },
+    {
+        src: "/png/sandClock.svg",
+        alt: ""
+    },
+    {
+        src: "/png/timer.svg",
+        alt: ""
+    },
+    {
+        src: "/png/question.svg",
+        alt: ""
+    },
+    {
+        src: "/png/lock.svg",
+        alt: ""
+    },
+    {
+        src: "/png/pictureIcon.png",
+        alt: ""
+    }
+]
+
+export default customLevelImages;
